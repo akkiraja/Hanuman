2bfe5ccb39b053306e7c7b924a51b48190142b0f
Akshay Vikhe <akshayvikhe@Akshays-MacBook-Pro.local>
2025-09-27 00:15:26 +0530

    Some update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
 
 // Storage keys for the app
 export const STORAGE_KEYS = {
-  AUTH_STORE: `chitfund-758cab24-6813-4245-b862-1c7cd49fd7aa-auth`,
-  CHIT_STORE: `chitfund-758cab24-6813-4245-b862-1c7cd49fd7aa-chit`,
-  USER_PREFERENCES: `chitfund-758cab24-6813-4245-b862-1c7cd49fd7aa-prefs`,
+  AUTH_STORE: `chitfund-1d6ed105-52c0-4995-bf9f-8fbcd055ed36-auth`,
+  CHIT_STORE: `chitfund-1d6ed105-52c0-4995-bf9f-8fbcd055ed36-chit`,
+  USER_PREFERENCES: `chitfund-1d6ed105-52c0-4995-bf9f-8fbcd055ed36-prefs`,
 };
 
 export const APP_CONFIG = {
-  PROJECT_ID: '758cab24-6813-4245-b862-1c7cd49fd7aa',
+  PROJECT_ID: '1d6ed105-52c0-4995-bf9f-8fbcd055ed36',
   MIN_GROUP_MEMBERS: 4,
   MAX_GROUP_MEMBERS: 40,
 };
